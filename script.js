83025fe21b039c048fb84d50fb18c6e2e00adc62
kristibyrnes <kristibyrnes@icloud.com>
2018-02-06 20:43:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+var thisTable;
+
+$(".available").on("click", function (event){
+    thisTable = $(this);
+    $("form").removeClass("hidden");
+    var tableNumber = $(this).text();
+    $("h3 > span").text(tableNumber);
+
+});
+
+  $("button").on("click", function (event){
+    event.preventDefault();
+    thisTable.addClass("reserved");
+    addTableData();
+    $('form').addClass("hidden");
+    $('form')[0].reset();
+  });
+
+  $("#x").click(function(){
+    $('form').addClass("hidden");
+  });
+
+  function addTableData(){
+    var nameInput = $("#partyName").val();
+    var partyInput = $("#partySize").val();
+
+    thisTable.append('<div class="fullInfo"><div> Name: ' + nameInput + '</div><div> Size of party: ' + partyInput + '</div')
+
+  }
+});
